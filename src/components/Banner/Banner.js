dce673d9dc6038c7834bace82d29c3c400e96151
Bajuddin Khan <bajuddinkhan786@gmail.com>
2022-11-08 14:57:29 +0530

    update banner title

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypto Tracker
           </Typography>
           <Typography
             variant="subtitle2"
